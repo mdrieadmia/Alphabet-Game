30d90a98ec9ddffd955f3546bf31895baae272bd
Riead <mdriead.bd@gmail.com>
2024-02-15 11:31:12 +0600

    Game function added

--- scripts/alphabet.js
+++ scripts/alphabet.js
@@ -1,5 +1,78 @@
+// Genarate a random letter
+function getRandomLetter(){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    const letter = alphabet.split('');
+    const randomLetter = letter[Math.round(Math.random()*25)];
+    return randomLetter;
+}
+// display random letter
+function displayRandomLetter(){
+    const display = document.getElementById('display_letter');
+    const randomLetter = getRandomLetter();
+    display.innerText = randomLetter;
+    setBackgroundColor(randomLetter);
+}
+// Set keyboard keys background color for random number
+function setBackgroundColor(letterId){
+    const randomKey = document.getElementById(letterId);
+    randomKey.classList.add('bg-orange-400');
+}
+// Set keyboard keys background color for random number
+function removeBackgroundColor(letterId){
+    const randomKey = document.getElementById(letterId);
+    randomKey.classList.remove('bg-orange-400');
+}
+// Continue Game
 function continueGame(){
     showElement('second_screen');
     hideElement('first_screen');
     displayRandomLetter();
+}
+// User keypress event
+function userKeyPressed(event){
+    const pressedKey = event.key;
+    const expectedKeyElement = document.getElementById('display_letter');
+    const expectedKeyText = expectedKeyElement.innerText;
+    const expectedKey = expectedKeyText.toLocaleLowerCase();
+    if(pressedKey === expectedKey){
+        displayRandomLetter();
+        removeBackgroundColor(pressedKey);
+        const scoreElement = document.getElementById('score');
+        const scoreText = scoreElement.innerText;
+        let score = parseInt(scoreText);
+        score++;
+        scoreElement.innerText = score;
+    }
+    else{
+        const lifeElement = document.getElementById('life');
+        const lifeText = lifeElement.innerText;
+        let life = parseInt(lifeText);
+        life--;
+        lifeElement.innerText = life;
+        if(life === 0 || event.key === 'Escape' || event.key === 'Enter'){
+            removeBackgroundColor(expectedKey);
+            hideElement('second_screen');
+            showElement('third_screen');
+            gameOver();
+        }
+    }
+}
+// Get user keyboard event
+document.addEventListener('keyup', userKeyPressed);
+
+function playAgain(){
+    hideElement('third_screen');
+    showElement('second_screen')
+    const scoreElement = document.getElementById('score');
+    scoreElement.innerText = '0';
+    const lifeElement = document.getElementById('life');
+    lifeElement.innerText = '5';
+    continueGame();
+}
+
+function gameOver(){
+    const lastScore = document.getElementById('lastScore');
+    const finalScore = document.getElementById('score').innerText;
+    const displayFinalScore = lastScore.innerText=finalScore;
+    return displayFinalScore;
 }
--- utility/utility.js
+++ utility/utility.js
@@ -8,22 +8,3 @@ function hideElement(elementId){
     const elementItem = document.getElementById(elementId);
     elementItem.classList.add('hidden')
 }
-// Genarate a random letter
-function getRandomLetter(){
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    const letter = alphabet.split('');
-    const randomLetter = letter[Math.round(Math.random()*25)];
-    return randomLetter;
-}
-// display random letter
-function displayRandomLetter(){
-    const display = document.getElementById('display_letter');
-    const randomLetter = getRandomLetter();
-    display.innerText = randomLetter;
-    setBackgroundColor(randomLetter);
-}
-// Set keyboard keys background color for random number
-function setBackgroundColor(letterId){
-    const randomKey = document.getElementById(letterId);
-    randomKey.classList.add('bg-orange-400');
-}
